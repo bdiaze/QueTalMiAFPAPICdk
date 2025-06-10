22972170b31c0e2d54a63250facb0ab1cf4af9c4
Benjamín <benjamin.diaze@outlook.com>
2025-06-10 18:31:20 -0400

    Se añaden singletons para los repositories

--- QueTalMiAFPAPI/Program.cs
+++ QueTalMiAFPAPI/Program.cs
@@ -1,14 +1,37 @@
+using QueTalMiAFPAPI.Helpers;
+using QueTalMiAFPAPI.Interfaces;
+using QueTalMiAFPAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string parameterArnApiAllowedDomains = Environment.GetEnvironmentVariable("PARAMETER_ARN_API_ALLOWED_DOMAINS") ?? throw new ArgumentNullException("PARAMETER_ARN_API_ALLOWED_DOMAINS");
+string[] allowedDomains = (await ParameterStore.ObtenerParametro(parameterArnApiAllowedDomains)).Split(",");
 
 builder.Services.AddControllers();
 
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
 
+builder.Services.AddCors(item => {
+    item.AddPolicy("CORSPolicy", builder => {
+        builder.WithOrigins(allowedDomains)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ConnectionString, ConnectionString>();
+builder.Services.AddSingleton<IComisionDAO, ComisionDAO>();
+builder.Services.AddSingleton<ICuotaDAO, CuotaDAO>();
+builder.Services.AddSingleton<ICuotaUfComisionDAO, CuotaUfComisionDAO>();
+builder.Services.AddSingleton<IMensajeUsuarioDAO, MensajeUsuarioDAO>();
+builder.Services.AddSingleton<ITipoMensajeDAO, TipoMensajeDAO>();
+builder.Services.AddSingleton<IUfDAO, UfDAO>();
 
 var app = builder.Build();
 
@@ -20,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CORSPolicy");
 
 app.UseAuthorization();
 
